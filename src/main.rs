023e0bbc69a32027f3ec1432e12a4182beeede04
Zekiz774 <gerlinge122@gmail.com>
2025-03-20 22:08:54 +0100

    Modified clock to be compatible with USB since the external oscilator uses a 25MHz crystal and USB needs 48MHz

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,57 @@
 #![no_std]
 #![no_main]
 
-use defmt::println;
+use defmt::{debug, println};
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Speed};
+use embassy_stm32::{
+    gpio::{AnyPin, Level, Output, Pin, Speed},
+    time::Hertz,
+    Config,
+};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(25_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll_src = PllSource::HSE;
+        config.rcc.pll = Some(Pll {
+            prediv: PllPreDiv::DIV25,  // 1Hz
+            mul: PllMul::MUL336,       // 336Hz
+            divp: Some(PllPDiv::DIV4), // 84Hz
+            divq: Some(PllQDiv::DIV6), //48Hz
+            divr: None,
+        });
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q;
+    };
+
+    let p = embassy_stm32::init(config);
+    debug!("test");
+
+    _spawner.spawn(blink(p.PC13.degrade())).unwrap();
+}
 
 #[embassy_executor::task]
 async fn blink(pin: AnyPin) {
     let mut led = Output::new(pin, Level::Low, Speed::High);
 
     loop {
-        println!("hello world");
+        debug!("hello world");
         led.set_level(Level::High);
         Timer::after_millis(150).await;
 
         led.set_level(Level::Low);
         Timer::after_millis(150).await;
     }
-}
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let pin = embassy_stm32::init(Default::default());
-    _spawner.spawn(blink(pin.PC13.degrade())).unwrap()
 }
